2a98f796649b54bd9598a778fffb9651f5601a73
mahesh353 <pritampatil353@gmail.com>
2025-01-10 09:53:26 +0530

    Code changes for episode-2

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-const heading = React.createElement("h1", {id:"heading", xyz:"some text"}, "Hellow World from React !");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", {id:"heading", xyz:"some text"}, "Hellow World from React  !");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
